fb5210298ef21079476b8d2c5f713ebe56eb8c82
Dung Bui <dungbui0403@gmail.com>
2025-02-27 09:41:06 +0700

    Update Login UI with Google and Facebook buttons

---
+++ flutter_app/lib/components/my_text_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget{
+  final controller;
+  final String hintText;
+  final bool obsecureText;
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obsecureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obsecureText,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color : Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          fillColor: Colors.grey.shade200,
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey[500]),
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_app/lib/pages/login_screen.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_app/components/my_button.dart';
+import 'package:flutter_app/components/my_text_field.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void signUserIn() {
+    // TODO: Thực hiện chức năng đăng nhập ở đây
+  }
+
+  void signInWithGoogle() {
+    // TODO: Thực hiện chức năng đăng nhập Google
+  }
+
+  void signInWithFacebook() {
+    // TODO: Thực hiện chức năng đăng nhập Facebook
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFFe0f7fa),
+                  Color(0xFFffffff),
+                ],
+              ),
+            ),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 50),
+
+                    // Text Login
+                    Text(
+                      'Welcome Back!',
+                      style: GoogleFonts.poppins(
+                        color: Colors.blue[900],
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Login to continue',
+                      style: GoogleFonts.poppins(
+                        color: Colors.grey[700],
+                        fontSize: 16,
+                      ),
+                    ),
+
+                    const SizedBox(height: 40),
+
+                    // Username TextField
+                    MyTextField(
+                      controller: usernameController,
+                      hintText: 'Username',
+                      obsecureText: false,
+                    ),
+
+                    const SizedBox(height: 25),
+
+                    // Password TextField
+                    MyTextField(
+                      controller: passwordController,
+                      hintText: 'Password',
+                      obsecureText: true,
+                    ),
+
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Forgot Password?',
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 25),
+
+                    // Button Login
+                    MyButton(
+                      onTap: signUserIn,
+                    ),
+
+                    const SizedBox(height: 30),
+
+                    // Or continue with
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Text('or continue with'),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 30),
+
+                    // Social Media Buttons
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: signInWithGoogle,
+                          child: Container(
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey.shade300),
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                            ),
+                            child: Row(
+                              children: [
+                                Image.asset(
+                                  'assets/icons/google.png',
+                                  height: 24,
+                                ),
+                                const SizedBox(width: 10),
+                                const Text('Google'),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 20),
+                        GestureDetector(
+                          onTap: signInWithFacebook,
+                          child: Container(
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey.shade300),
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                            ),
+                            child: Row(
+                              children: [
+                                Image.asset(
+                                  'assets/icons/facebook.png',
+                                  height: 24,
+                                ),
+                                const SizedBox(width: 10),
+                                const Text('Facebook'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 50),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
